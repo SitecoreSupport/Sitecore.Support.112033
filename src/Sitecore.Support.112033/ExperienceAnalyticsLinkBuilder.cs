ad6fdc4c03de4de868434056497ecc0ecce97800
dk\ivd <ivd@sitecore.net>
2018-11-22 11:02:49 +0800

    Make ExperienceAnalyticsLinkBuilder generate links for specific site

---
+++ src/Sitecore.Support.112033/ExperienceAnalyticsLinkBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Marketing.Definitions.Campaigns;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.Marketing.Campaigns.Services
+{
+  public class ExperienceAnalyticsLinkBuilder : Sitecore.Marketing.Campaigns.Services.ExperienceAnalyticsLinkBuilder
+  {
+    [CanBeNull]
+    string _contextSiteName;
+
+    public ExperienceAnalyticsLinkBuilder([CanBeNull] string contextSiteName)
+    {
+      _contextSiteName = contextSiteName;
+    }
+    public override string GetReportLink(ICampaignActivityDefinition campaignActivityDefinition)
+    {
+      if (string.IsNullOrEmpty(_contextSiteName))
+      {
+        return base.GetReportLink(campaignActivityDefinition);
+      }
+
+      // To minimize overriden code
+      using (new SiteContextSwitcher(SiteContextFactory.GetSiteContext(_contextSiteName)))
+      {
+        return base.GetReportLink(campaignActivityDefinition);
+      }
+    }
+
+    protected virtual SiteContext GetSiteContext()
+    {
+      return SiteContextFactory.GetSiteContext(_contextSiteName);
+    }
+  }
+}
