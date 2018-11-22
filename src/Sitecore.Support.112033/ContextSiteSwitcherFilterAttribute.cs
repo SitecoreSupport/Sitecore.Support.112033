27111d153372663d616d54b54954042bc29c02af
dk\ivd <ivd@sitecore.net>
2018-11-22 10:44:11 +0800

    Implement context site switcher filter

---
+++ src/Sitecore.Support.112033/ContextSiteSwitcherFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.Marketing.Campaigns.Services.Filters
+{
+    internal sealed class ContextSiteSwitcherFilterAttributeAttribute : ActionFilterAttribute
+    {
+        [NotNull]
+        readonly string _siteName;
+
+        public ContextSiteSwitcherFilterAttributeAttribute([NotNull] string siteName)
+        {
+            Assert.ArgumentNotNullOrEmpty(siteName, nameof(siteName));
+
+            _siteName = siteName;
+        }
+
+        public override void OnActionExecuting(HttpActionContext filterContext) =>
+            SiteContextSwitcher.Enter(Factory.GetSite(_siteName));
+
+
+        public override void OnActionExecuted(HttpActionExecutedContext filterContext) =>
+            SiteContextSwitcher.Exit();
+    }
+}
