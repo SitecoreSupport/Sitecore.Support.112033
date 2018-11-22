ea79a6dbdffbf747ea47081b7340ce5347f4a2b3
dk\ivd <ivd@sitecore.net>
2018-11-22 10:57:38 +0800

    Override CampaignController

---
+++ src/Sitecore.Support.112033/CampaignController.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Marketing.Campaigns.Services.Attributes;
+using Sitecore.Marketing.Campaigns.Services.Data;
+using Sitecore.Marketing.Campaigns.Services.Model;
+using Sitecore.Services.Core;
+using Sitecore.Services.Core.ComponentModel.DataAnnotations;
+using Sitecore.Services.Infrastructure.Services;
+using Sitecore.Web.Http.Filters;
+
+namespace Sitecore.Support.Marketing.Campaigns.Services.Controllers
+{
+  [ServicesController("CampaignManagement.Campaign")]
+  [ArgumentExceptionFilter]
+  [AccessDeniedExceptionFilter]
+  [UnauthorizedAccessExceptionFilter]
+  [ValidateHttpAntiForgeryToken]
+  [SitecoreAuthorize(Roles = SitecoreAnalyticsMaintaining + "," + SitecoreAnalyticsReporting)]
+  public class CampaignController : Sitecore.Marketing.Campaigns.Services.Controllers.CampaignController
+  {
+    const string SitecoreAnalyticsMaintaining = "sitecore\\Analytics Maintaining";
+
+    const string SitecoreAnalyticsReporting = "sitecore\\Analytics Reporting";
+
+    public CampaignController() : base()
+    {
+    }
+
+    public CampaignController(IFilteredRepository<CampaignEntity> repository) : base(repository)
+    {
+    }
+
+    public CampaignController(IFilteredRepository<CampaignEntity> repository, IMetaDataBuilder metadataBuilder, IEntityValidator entityValidator) : base(repository, metadataBuilder, entityValidator)
+    {
+    }
+  }
+}
