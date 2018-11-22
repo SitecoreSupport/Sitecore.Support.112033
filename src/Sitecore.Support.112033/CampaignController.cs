7cb7ee1b64ca3357ef33070a4e505af6b1838c84
dk\ivd <ivd@sitecore.net>
2018-11-22 11:05:26 +0800

    Inject custom ExperienceAnalyticsLinkBuilder

--- src/Sitecore.Support.112033/CampaignController.cs
+++ src/Sitecore.Support.112033/CampaignController.cs
@@ -6,6 +6,9 @@ using Sitecore.Services.Core.ComponentModel.DataAnnotations;
 using Sitecore.Services.Infrastructure.Services;
 using Sitecore.Support.Marketing.Campaigns.Services.Filters;
 using Sitecore.Web.Http.Filters;
+using System.Reflection;
+
+using CampaignRepositoryType = Sitecore.Marketing.Campaigns.Services.Data.CampaignRepository;
 
 namespace Sitecore.Support.Marketing.Campaigns.Services.Controllers
 {
@@ -22,16 +25,36 @@ namespace Sitecore.Support.Marketing.Campaigns.Services.Controllers
 
     const string SitecoreAnalyticsReporting = "sitecore\\Analytics Reporting";
 
+    const string EASiteName = "website";
+
+    readonly static PropertyInfo _mLinkBuilder = typeof(CampaignRepositoryType)
+        .GetProperty("LinkBuilder", BindingFlags.NonPublic | BindingFlags.Instance);
+
     public CampaignController() : base()
     {
+      Init();
     }
 
     public CampaignController(IFilteredRepository<CampaignEntity> repository) : base(repository)
     {
+      Init();
     }
 
     public CampaignController(IFilteredRepository<CampaignEntity> repository, IMetaDataBuilder metadataBuilder, IEntityValidator entityValidator) : base(repository, metadataBuilder, entityValidator)
     {
+      Init();
+    }
+
+    void Init()
+    {
+      var campaignRepository = Repository as CampaignRepositoryType;
+
+      if (campaignRepository == null)
+      {
+        return;
+      }
+
+      _mLinkBuilder.SetValue(campaignRepository, new ExperienceAnalyticsLinkBuilder(EASiteName));
     }
   }
 }
